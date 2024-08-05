41cf2652da2d8c934edd106f411fbe1cae23d9b4
Selene Tamez Galván <tg.sln255@gmail.com>
2024-08-04 18:20:36 -0600

    Define arrayToNotation()

--- main.js
+++ main.js
@@ -8,3 +8,11 @@ function notationToArray(chessSquare) {
   const y = +chessSquare.charAt(1) - 1;
   return [x, y];
 }
+
+function arrayToNotation(array) {
+  const xCodePoint = array[0] + 97;
+  const sqLetter = String.fromCodePoint(xCodePoint);
+  const yCodePoint = array[1] + 49;
+  const sqNumber = String.fromCodePoint(yCodePoint);
+  return `${sqLetter}${sqNumber}`;
+}
