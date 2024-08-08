95f2e8e200d79b96ea3dbfa02c7f60f37bc01c78
Selene Tamez Galván <tg.sln255@gmail.com>
2024-08-08 15:31:02 -0600

    Cache valid knight moves for each chess square

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import validMoves from "./valid-moves.js";
+import validMoves from "./valid-moves.js";
+
 function notationToArray(chessSquare) {
   if (chessSquare.length !== 2) {
     throw new Error("A square is represented by two characters only.");
@@ -17,30 +20,34 @@ function arrayToNotation(array) {
   return `${sqLetter}${sqNumber}`;
 }
 
-function isValidMove(moveArr) {
-  const [x, y] = moveArr;
-  return x >= 0 && x < 8 && y >= 0 && y < 8;
-}
+// function isValidMove(moveArr) {
+//   const [x, y] = moveArr;
+//   return x >= 0 && x < 8 && y >= 0 && y < 8;
+// }
 
-function processAllValidMoves() {
-  const validMoves = [];
-  for (let x = 0; x < 7; x++) {
-    validMoves[x] = [];
-    for (let y = 0; y < 7; y++) {
-      validMoves[x][y] = [];
-      let moveDifMatrix = [[2, 1], [2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2], [-2, 1], [-2, -1]];
-      for (const move of moveDifMatrix) {
-        const knightMove = [x + move[0], y + move[1]];
-        if (isValidMove(knightMove)) {
-          validMoves[x][y].push(knightMove);
-        }
-      }
-    }
-  }
-  return validMoves;
-}
+// function processAllValidMoves() {
+//   const validMoves = [];
+//   for (let x = 0; x < 7; x++) {
+//     validMoves[x] = [];
+//     for (let y = 0; y < 7; y++) {
+//       validMoves[x][y] = [];
+//       let moveDifMatrix = [[2, 1], [2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2], [-2, 1], [-2, -1]];
+//       for (const move of moveDifMatrix) {
+//         const knightMove = [x + move[0], y + move[1]];
+//         if (isValidMove(knightMove)) {
+//           validMoves[x][y].push(knightMove);
+//         }
+//       }
+//     }
+//   }
+//   return validMoves;
+// }
 
 function knightMoves(startSq, destinationSq) {
   const startArr = notationToArray(startSq);
   const destinationArr = notationToArray(destinationSq);
 }
+
+const validMoves = validMoves();
+
+console.log(JSON.stringify(processAllValidMoves()));
